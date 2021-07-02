0e29aa037b76d5f388e07a83eeed869b07cf61ed
Olivier <olivier@plankdesign.com>
2021-07-02 19:40:46 -0400

    add open graph

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,10 @@ export default function Home() {
         <meta name="robots" content="all" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fffff9"/>
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1b191f"/>
+        <meta property="og:title" content="Olivier Carignan | Front-End Developer &amp; UX / UI Designer" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="💻" />
+        <meta property="og:url" content="https://www.oliviercarignan.dev" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>"/>
       </Head>
       <div className="header flex justify-between items-center">
